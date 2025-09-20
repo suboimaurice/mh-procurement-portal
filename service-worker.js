a40a1e1ebfa6a298ede900cea95c4255b5c824a9
Suboi Maurice <suboimaurice64@gmail.com>
2025-09-20 01:14:18 +0000

    changes made to the html files and manifest.json to improve SEO and accessibility.

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,31 @@
 const CACHE_NAME = 'mh-procurement-cache-v1';
 const urlsToCache = [
-  '/', // Cache the root
-  '/index.html', // Cache the main index.html
-  '/manifest.json',
-  '/pages/about.html',
-  '/pages/contact.html',
-  '/pages/medical-supplies.html',
-  '/pages/office-supplies.html',
-  '/pages/pharmaceuticals.html',
-  '/pages/services.html',
-  '/pages/how-it-works.html',
-  '/pages/lab-equipment.html',
-  '/pages/emergency-contact.html',
-  '/commons/header.html',
-  '/commons/footer.html',
+  './', // Cache the root
+  './index.html', // Cache the main index.html
+  './manifest.json',
+  './pages/about.html',
+  './pages/contact.html',
+  './pages/medical-supplies.html',
+  './pages/office-supplies.html',
+  './pages/pharmaceuticals.html',
+  './pages/services.html',
+  './pages/how-it-works.html',
+  './pages/lab-equipment.html',
+  './pages/emergency-contact.html',
+  './commons/header.html',
+  './commons/footer.html',
 
-  '/assets/css/style.css',
-  '/assets/css/output.css',
-  '/assets/js/index.js',
-  '/assets/images/logo.png',
-  '/assets/images/icons/icon-72x72.png',
-  '/assets/images/icons/manifest-icon-192.maskable.png',
-  '/assets/images/icons/manifest-icon-512.maskable.png',
+  './assets/css/style.css',
+  './assets/css/output.css',
+  './assets/js/index.js',
+  './assets/images/logo.png',
+  './assets/images/icons/icon-72x72.png',
+  './assets/images/icons/manifest-icon-192.maskable.png',
+  './assets/images/icons/manifest-icon-512.maskable.png',
 
     // Screenshots for PWA install prompt
-  '/assets/images/screenshots/homepage-wide.png',
-  '/assets/images/screenshots/homepage-mobile.png',
+  './assets/images/screenshots/homepage-wide.png',
+  './assets/images/screenshots/homepage-mobile.png',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
 
 ];
